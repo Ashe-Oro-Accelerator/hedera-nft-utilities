c8161b4d09ed1abe0ffd88d50d668095b3655520
michalrozekariane <michal.rozek@arianelabs.com>
2024-02-21 12:36:19 +0100

    feat: task 26 moved from old repo
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

---
+++ src/helpers/uri-decoder.ts
@@ -0,0 +1,33 @@
+import { NFTDetails, DecodedMetadata } from '../types/nfts.module';
+import { dictionary } from '../utils/constants/dictionary';
+import { errorToMessage } from './error-to-message';
+
+export const uriDecoder = (nfts: NFTDetails | NFTDetails[], ipfsGateway?: string): DecodedMetadata[] => {
+  const nftsArray = Array.isArray(nfts) ? nfts : [nfts];
+  const decodedMetadataArray: DecodedMetadata[] = nftsArray.map((nft: NFTDetails) => {
+    let decodedNFTMetadata: string = '';
+
+    try {
+      decodedNFTMetadata = atob(nft.metadata);
+    } catch (error) {
+      throw new Error(errorToMessage(error));
+    }
+
+    if (!decodedNFTMetadata.startsWith('https://') && !decodedNFTMetadata.startsWith('http://') && !ipfsGateway) {
+      throw new Error(dictionary.errors.ipfsGatewayRequired);
+    }
+
+    if (decodedNFTMetadata.startsWith('ipfs://') && ipfsGateway) {
+      decodedNFTMetadata = decodedNFTMetadata.replace('ipfs://', ipfsGateway);
+    } else if (!decodedNFTMetadata.startsWith('https://') && !decodedNFTMetadata.startsWith('http://') && ipfsGateway) {
+      decodedNFTMetadata = `${ipfsGateway}${decodedNFTMetadata}`;
+    }
+
+    return {
+      metadata: decodedNFTMetadata,
+      serialNumber: nft.serial_number,
+    };
+  });
+
+  return decodedMetadataArray;
+};
--- src/nftSDKFunctions/create-json-metadata-from-csv.ts
+++ src/nftSDKFunctions/create-json-metadata-from-csv.ts
@@ -1,51 +1,7 @@
-/*-
- *
- * Hedera NFT Utilities
- *
- * Copyright (C) 2024 Hedera Hashgraph, LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-import { dictionary } from '../utils/constants/dictionary';
-import { Hip412Metadata } from '../utils/hedera/hip412-metadata';
-import { CSVRowAsObject } from '../types/csv.module';
-import { errorToMessage } from '../helpers/error-to-message';
-import { JsonMetadataFromCSVInterface } from '../types/json-metadata-from-csv.module';
 import { CSVFileReader } from '../csv-file-reader';
 import { JsonMetadataFromCSVConverter } from '../services/json-metadata-from-csv-converter';
-
-const validateMetadataObjects = (
-  metadataObjectsFromCSVRows: CSVRowAsObject[],
-  csvFilePath: string
-): { metadataObjectsValidationErrors: string[]; missingAttributesErrors: string[] } => {
-  const metadataObjectsValidationErrors: string[] = [];
-  const missingAttributesErrors: string[] = [];
-
-  for (const [index, metadataObject] of metadataObjectsFromCSVRows.entries()) {
-    try {
-      Hip412Metadata.validateMetadataFromCSV(metadataObject);
-    } catch (e) {
-      metadataObjectsValidationErrors.push(dictionary.csvToJson.errorInRow(index + 1, errorToMessage(e)));
-    }
-
-    if (!metadataObject.attributes) {
-      missingAttributesErrors.push(dictionary.csvToJson.missingAttributesInRow(csvFilePath, index + 1));
-    }
-  }
-
-  return { metadataObjectsValidationErrors, missingAttributesErrors };
-};
+import { JsonMetadataFromCSVInterface } from '../types/json-metadata-from-csv.module';
+import { Hip412Validator } from '../hip412-validator';
 
 export const createJsonMetadataFromCSV = async ({
   savedJsonFilesLocation,
@@ -67,15 +23,15 @@ export const createJsonMetadataFromCSV = async ({
     headerProperties: CSVFileReader.PROPERTIES,
   });
 
-  const { metadataObjectsValidationErrors, missingAttributesErrors } = validateMetadataObjects(metadataObjectsFromCSVRows, csvFilePath);
+  const { isValid, errors } = Hip412Validator.validateArrayOfObjects(metadataObjectsFromCSVRows, csvFilePath);
 
-  JsonMetadataFromCSVConverter.saveCSVRowsAsJsonFiles(metadataObjectsFromCSVRows, savedJsonFilesLocation);
+  if (isValid) {
+    JsonMetadataFromCSVConverter.saveCSVRowsAsJsonFiles(metadataObjectsFromCSVRows, savedJsonFilesLocation);
+  }
 
   return {
-    errors: {
-      metadataObjectsValidationErrors,
-      missingAttributesErrors,
-    },
+    isValid,
+    errors,
     savedJsonFilesLocation,
   };
 };
